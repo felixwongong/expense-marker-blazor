3c084590641e6ad656c307b0701d2d18fb95d169
Felix Wong <74048524+felixwongong@users.noreply.github.com>
2025-02-15 19:23:22 +0800

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace expense_marker_blazor;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace expense_marker_blazor;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
